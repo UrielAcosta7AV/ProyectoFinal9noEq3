3027a956a5e0696d7097b3589217b4c02365928e
UPBC <viperezf@upbc.edu.mx>
2015-08-17 21:55:36 -0700

    Elimina archivos inecesarios

--- cleanFolder.bat
+++ cleanFolder.bat
@@ -8,6 +8,17 @@ del /s /q *.log
 del /s /q *.idb
 del /s /q *.tlog
 del /s /q *.lastbuildstate
+del /s /q *.apk
+del /s /q *.o
+del /s /q *.db
+for /f "usebackq delims=" %%d in (`"dir /ad/b/s | sort /R"`) do rd "%%d"
 rmdir /s /q Debug
 rmdir /s /q Release
+rmdir /s /q generated
+rmdir /s /q intermediates
+rmdir /s /q outputs
+rmdir /s /q tmp
+rmdir /s /q androidTest
+rmdir /s /q obj
+rmdir /s /q libs
 pause
